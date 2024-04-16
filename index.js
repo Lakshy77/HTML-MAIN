2a6e2254b45ce0b06ee6f90fa7d8ecf84f88f3a5
Lakshy77 <lakshyjain77@gmail.com>
2024-04-16 21:06:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//Replace the arguments below
+//space,scary,military,romantic,cowboy,fantasy,superhero
+favouriteMovieGenre("space")
+//watermelon,tomato,banana,orange,avocado,blueberry
+favouriteFruit("orange")
+//light,dark
+favouriteMode("dark")
+//sharp,soft,round
+favouriteEdgeStyle("sharp")
